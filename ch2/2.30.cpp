96ab27a4ad3dcd6ba22be3f66ee8e84fb09cf805
Daemonumen <145369560+Daemonumen@users.noreply.github.com>
2024-03-20 17:12:52 +0100

    Update 2.30.cpp
    miaw

--- ch2/2.30.cpp
+++ ch2/2.30.cpp
@@ -1,6 +1,6 @@
 int main() {
   int i;
-  const int v2 = 0;  // v2 has top-level const
+  const int v2 = 0;  // v2 has low-level const
   int v1 = v2;
   int *p1 = &v1, &r1 = v1;
   const int *p2 = &v2, *const p3 = &i, &r2 = v2;  // p2 has low-level const
